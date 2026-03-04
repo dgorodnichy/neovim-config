b5103ef2028fa94b16325834d3291ec609d7c149
Dmitry Gorodnichy <gorodnichiydv@psblab.ru>
2026-03-04 19:25:55 +0300

    Refactoring

--- init.lua
+++ init.lua
@@ -1,8 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-vim.opt.signcolumn = "yes"
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 16
-vim.opt.cursorline = true
-
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,7 +5,7 @@
 vim.opt.relativenumber = false
 vim.opt.termguicolors = true
 vim.g.autoformat = false
-vim.lsp.set_log_level("WARN")
+vim.lsp.set_log_level(vim.log.levels.WARN)
 
 vim.g.root_spec = { "cwd" }
 
@@ -13,6 +13,11 @@ vim.g.lazyvim_ruby_lsp = "ruby_lsp"
 vim.g.lazyvim_ruby_formatter = "rubocop"
 
 vim.opt.clipboard = "unnamedplus"
+
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 16
+vim.opt.cursorline = true
 
 vim.keymap.set({ "n", "v", "x" }, "<D-c>", '"+y', { noremap = true, silent = true })
 vim.keymap.set({ "n", "v", "x" }, "<D-x>", '"+d', { noremap = true, silent = true })
